e8c2c9a2f6dfe2f3149b7fcd257416ad505961b9
SoniaGrh <sonia.garrouch@hotmail.fr>
2023-04-03 12:45:09 +0000

    add yolov3 images

--- dataset/build_images.sh
+++ dataset/build_images.sh
@@ -1,5 +1,9 @@
 docker build -f Dockerfile.tensorflow . -t picsellpn/preannotation-tf:capture
 docker push picsellpn/preannotation-tf:capture 
 
+docker build -f Dockerfile.tensorflow . -t picsellpn/preannotation-yolov3:capture
+docker push picsellpn/preannotation-yolov3:capture 
+
 docker build -f Dockerfile.yolov8 . -t picsellpn/preannotation-yolov8:capture
 docker push picsellpn/preannotation-yolov8:capture 
+

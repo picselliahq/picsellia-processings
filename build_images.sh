06f77863d47b0d77b946b3f672af59cb7b2e6bd1
SoniaGrh <sonia.garrouch@hotmail.fr>
2023-04-06 15:21:53 +0000

    update processings

---
+++ build_images.sh
@@ -0,0 +1,9 @@
+docker build -f docker/Dockerfile.tensorflow . -t picsellpn/preannotation-tf:capture
+docker push picsellpn/preannotation-tf:capture 
+
+docker build -f docker/Dockerfile.tensorflow . -t picsellpn/preannotation-yolov3:capture
+docker push picsellpn/preannotation-yolov3:capture 
+
+docker build -f docker/Dockerfile.yolov8 . -t picsellpn/preannotation-yolov8:capture
+docker push picsellpn/preannotation-yolov8:capture 
+

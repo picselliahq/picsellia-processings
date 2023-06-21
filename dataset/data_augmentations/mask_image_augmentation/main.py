a77497bf5d2c102ec75a70c289b57cad3379e89e
hajeressef <hajer.essefi@picsellia.com>
2023-06-21 18:42:26 +0200

    fix output dataset name

--- dataset/data_augmentations/mask_image_augmentation/main.py
+++ dataset/data_augmentations/mask_image_augmentation/main.py
@@ -41,6 +41,8 @@ job = client.get_job_by_id(job_id)
 
 context = job.sync()["dataset_version_processing_job"]
 input_dataset_version_id = context["input_dataset_version_id"]
+output_dataset_version = context["output_dataset_version_id"]
+
 parameters = context["parameters"]
 
 input_dataset_version: DatasetVersion = client.get_dataset_version_by_id(
@@ -84,13 +86,11 @@ for img in coco.imgs.values():
     augmented_image_paths.append(transformed_image_path)
 
 dataset = client.get_dataset_by_id(input_dataset_version.origin_id)
-new_dataset_name = input_dataset_version.version + "-augmented"
-augmented_dataset = dataset.create_version(new_dataset_name)
-create_data_and_add_to_dataset(augmented_image_paths, augmented_dataset)
 
-dataset_version_id = augmented_dataset.id
-dataset = client.get_dataset_version_by_id(dataset_version_id)
-dataset.set_type(InferenceType.SEGMENTATION)
+output_dataset: DatasetVersion = client.get_dataset_version_by_id(
+    output_dataset_version
+)
+create_data_and_add_to_dataset(augmented_image_paths, output_dataset)
 
 original_mask_directory = augmented_mask_dir
 data_directory = augmented_image_dir
@@ -98,4 +98,4 @@ data_directory = augmented_image_dir
 prepare_mask_directories_for_multilabel(class_to_pixel_mapping=class_to_pixel_mapping,
                                         mask_directory=original_mask_directory)
 
-convert_seperated_multiclass_masks_to_polygons(data_directory=data_directory, dataset_version=dataset)
+convert_seperated_multiclass_masks_to_polygons(data_directory=data_directory, dataset_version=output_dataset)

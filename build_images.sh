8ae6d3fd50dc5a571a22a9696844f8939bd731b7
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-02-14 12:13:09 +0100

    add build and push of vlm_tagging

--- build_images.sh
+++ build_images.sh
@@ -9,3 +9,6 @@ docker push picsellpn/preannotation-yolov8:capture
 
 docker build -f docker/Dockerfile.mask . -t picsellpn/albumentation-segmentation:capture
 docker push picsellpn/albumentation-segmentation:capture
+
+docker build -f docker/Dockerfile.vlm_tagging . -t picsellia/vlm-tagging:capture
+docker push picsellia/vlm-tagging:capture
